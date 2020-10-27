8826976bb85fc338d81990708100cab037180e2a
randythao <50189800+randythao@users.noreply.github.com>
2020-10-26 21:59:16 -0500

    Update linux-domainjoin.sh

--- linux-domainjoin.sh
+++ linux-domainjoin.sh
@@ -9,21 +9,3 @@
 # update package lists
 sudo apt-get update -y
 sudo apt-get install krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli -y
-
-echo $3 | kinit -V $2
-echo $3 | sudo realm join --verbose $1 -U "$2" --install=/
-
-sudo systemctl enable sssd
-sudo systemctl start sssd
-states="null"
-states1="null"
-grouPs="null"
-therealm="null"
-cauth="null"
-
-sssd_conf_path=/etc/sssd/sssd.conf
-
-sed -i 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
-sed -i 's/fallback_homedir = \/home\/%u@%d/#fallback_homedir = \/home\/%u@%d/g' /etc/sssd/sssd.conf
-
-sudo service sssd restart
